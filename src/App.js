b683313b49873405f3f0b1baa22947010c5ea656
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-03-30 22:55:04 +0000

    retreive data & display tables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import EmployeeTable from './components/EmployeeTable'
+import CustomerTable from './components/CustomerTable'
+import InvoiceTable from './components/InvoiceTable'
 
 function App() {
+  const [company, setCompany] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [showCustomers, setCustomers] = useState(false);
+  const [showInvoices, setInvoices] = useState(false);
+  const [showEmployee, setEmployees] = useState(false);
+
+  const getData = async () => {
+    setLoading(true);
+    var request_url = "https://localhost:5001/api/company/all"
+    const request = await fetch(request_url);
+    const response = await request.json();
+    const data = response
+    setCompany(data);
+    setLoading(false);
+    console.log(data)
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+  const toggleEmployees = () => {
+    setEmployees(true)
+    setCustomers(false)
+    setInvoices(false)
+  }
+  const toggleCustomers = () => {
+    setCustomers(true)
+    setEmployees(false)
+    setInvoices(false)
+  }
+  const toggleInvoices = () => {
+    setInvoices(true)
+    setEmployees(false)
+    setCustomers(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <pre>{JSON.stringify(company[0].customers, undefined, 2)}</pre> */}
+      <div className="area">
+        <div className="group_of_btns">
+          <button onClick={toggleEmployees}>Get all Employees</button>
+          <button onClick={toggleCustomers}>Get all Customers</button>
+          <button onClick={toggleInvoices}>Get all Invoices</button>
+        </div>
+        <div className="table_area">
+          {loading === false && showEmployee ?
+            <EmployeeTable data={company[0].employees} show={showEmployee} />
+            : null
+          }
+          {loading === false && showCustomers ?
+            <CustomerTable data={company[0].customers} show={showCustomers} />
+            : null
+          }
+          {loading === false && showInvoices ?
+            <InvoiceTable data={company[0].invoices} show={showInvoices} />
+            : null
+          }
+        </div>
+      </div>
     </div>
   );
 }
